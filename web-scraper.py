79d22d3af965953f2fc24bdd4dc114585dc630bf
Daviey52 <davidkariska@gmail.com>
2022-02-04 11:03:03 -0800

    web scraper

--- web-scraper.py
+++ web-scraper.py
@@ -1,2 +1,27 @@
+
 import requests
 from bs4 import BeautifulSoup
+
+def  get_citations_needed_count(URL):
+  page = requests.get(URL)
+  soup = BeautifulSoup(page.content,'html.parser')
+  results = soup.find_all(class_="noprint Inline-Template Template-Fact")
+  return len(results)
+get_citations_needed_count(URL='https://en.wikipedia.org/wiki/Gene_therapy')
+
+def get_citations_needed_report(URL):
+  page = requests.get(URL)
+  contents = page.content
+  soup = BeautifulSoup(page.content,'html.parser')
+  #results = soup.find_all(class_="noprint Inline-Template Template-Fact")
+  results2 = soup.find_all(class_="noprint Inline-Template Template-Fact")
+  results = soup.find_all('p')
+
+  if results2 and results != ('sup id'):
+    for result in results:
+      print(result)
+
+
+get_citations_needed_report(URL='https://en.wikipedia.org/wiki/Gene_therapy')
+
+
