81e3966dfa05b03496f2bd121666a71713f7e1ad
Daviey52 <davidkariska@gmail.com>
2022-07-09 11:36:00 -0700

    updates

--- scrapper/web-scraper.py
+++ scrapper/web-scraper.py
@@ -1,11 +1,13 @@
 
-import requests
+from sys import prefix
+import requests , openpyxl
 from bs4 import BeautifulSoup
 
 def  get_citations_needed_count(URL):
   page = requests.get(URL)
   soup = BeautifulSoup(page.content,'html.parser')
   results = soup.find_all(class_="noprint Inline-Template Template-Fact")
+  print (len(results))
   return len(results)
 get_citations_needed_count(URL='https://en.wikipedia.org/wiki/Gene_therapy')
 
@@ -13,15 +15,15 @@ def get_citations_needed_report(URL):
   page = requests.get(URL)
   contents = page.content
   soup = BeautifulSoup(page.content,'html.parser')
-  #results = soup.find_all(class_="noprint Inline-Template Template-Fact")
-  results2 = soup.find_all(class_="noprint Inline-Template Template-Fact")
-  results = soup.find_all('p')
+  results = soup.find_all(class_="noprint Inline-Template Template-Fact")
+  res = ''
+  prefix = 'Citation needed for :'
 
-  if results2 and results != ('sup id'):
-    for result in results:
-      print(result)
-
-
+  for result in results:
+    parents = result.find_parents('p')
+    for parent in parents:
+      res += prefix + parent.text + '\n'
+      print(res)
 get_citations_needed_report(URL='https://en.wikipedia.org/wiki/Gene_therapy')
 
 
